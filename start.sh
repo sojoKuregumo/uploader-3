c09de8824a43d02aa651214aa9a2a6b3973a51ab
Kuregumo sojo <s26765549@gmail.com>
2026-01-14 19:26:13 +0530

    Modify MegaCMD installation instructions in start.sh
    Updated installation instructions for MegaCMD to indicate no sudo is needed on Render.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 1. Update and install MegaCMD
+# 1. Update and install MegaCMD (No sudo needed on Render)
 wget -q https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb
-sudo apt-get update -qq && sudo apt-get install -qq ./megacmd-xUbuntu_22.04_amd64.deb -y
+apt-get update -qq && apt-get install -qq ./megacmd-xUbuntu_22.04_amd64.deb -y
 
 # 2. Run the main Python script
 python3 main.py
