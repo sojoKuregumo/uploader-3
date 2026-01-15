339f99ba4b390059e64079ec0232403f308bfdc4
Kuregumo sojo <s26765549@gmail.com>
2026-01-15 08:49:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,12 @@
 # 1. Download MegaCMD
 wget -q https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megacmd-xUbuntu_22.04_amd64.deb
 
-# 2. Install using dpkg (Force installation without sudo)
-# Render allows dpkg if dependencies are already in the base image
-dpkg -x megacmd-xUbuntu_22.04_amd64.deb .
-export PATH=$PATH:$(pwd)/usr/bin
+# 2. Extract it locally (Bypassing sudo/root requirement)
+dpkg -x megacmd-xUbuntu_22.04_amd64.deb ./mega_local
 
-# 3. Start the bot
+# 3. Add the local bin folder to the system PATH so python can find 'mega-login'
+export PATH="$PWD/mega_local/usr/bin:$PATH"
+export LD_LIBRARY_PATH="$PWD/mega_local/usr/lib:$LD_LIBRARY_PATH"
+
+# 4. Start the bot
 python3 main.py
